f9d76fc2181ac2c71d86ce5df9b5f7651f6e309c
Nicolas Hiot <nicolas.hiot@etu.univ-orleans.fr>
2018-12-25 16:49:51 +0100

    Add simple geometrie support

--- src/app/healthy-api/class/structure.ts
+++ src/app/healthy-api/class/structure.ts
@@ -1,9 +1,10 @@
 import {StructureType} from './structure-type.enum';
 
 export class Structure {
-	public id?: number;
-	protected geometry: string; // GeoJSON string
+    public id?: number;
+    public lat?: number;
+    public lng?: number;
 
-	protected constructor(public readonly structure_type: StructureType, public name: string, public description: string = '') {
-	}
+    protected constructor(public readonly structure_type: StructureType, public name: string, public description: string = '') {
+    }
 }
